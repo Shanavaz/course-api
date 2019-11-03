5c5bcc47823883b32a9f2dfe164a772c845b4375
Shanu <shan.softwaresolutions@gmail.com>
2019-11-03 13:58:36 +0530

    Changes

--- course-api/src/main/java/io/javabrains/springbootstarter/hello/HelloController.java
+++ course-api/src/main/java/io/javabrains/springbootstarter/hello/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+	//changees
 	
 	@RequestMapping("/hello")
 	public String sayHi() {
